46d9d713e7a7baa4c5507ac524da80c5314d31ef
Alexnerud <a.ivanov23021989@gmail.com>
2025-06-03 12:03:22 +0300

    Исправляет ошибки в functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,18 @@
 const checkString = (string = '', maxLength = 1) => string.length <= maxLength;
-
-console.log(checkString('проверяемая строка', 20));
-console.log(checkString('проверяемая строка', 18));
-console.log(checkString('проверяемая строка', 10));
-
+checkString('проверяемая строка', 20);
+// console.log(checkString('проверяемая строка', 20));
+// console.log(checkString('проверяемая строка', 18));
+// console.log(checkString('проверяемая строка', 10));
 
 const isPalindrom = (string) => {
   string = string.replaceAll(' ', '').toLowerCase();
   let reversed = '';
-  for (i = string.length -1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     reversed += string[i];
   }
   return string === reversed;
 };
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
+isPalindrom('топот');
+// console.log(isPalindrom('топот'));
+// console.log(isPalindrom('ДовОд'));
+// console.log(isPalindrom('Кекс'));
